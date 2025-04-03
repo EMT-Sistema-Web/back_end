41783cef35eb1d98062c805edcbdb7b27fb07217
taisbarbosa <taisbarbosarodrigues2004gmail.com>
2025-04-03 12:04:39 -0300

    novo dado da machine

--- machine/models.py
+++ machine/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Machine(models.Model):
     code = models.CharField(max_length=200)
     reg_date = models.DateTimeField("date registered")
+    time_use = models.DateTimeField("date registered")
     status = models.BooleanField(default=False)
